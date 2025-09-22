9fbad69f9cdbb1a185fafdfc88cee4dc4fc2f1be
Diego Massarni <webdiegomassarini@gmail.com>
2025-09-22 23:05:56 +0200

    feat:add animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,13 @@ export default function Loading() {
   setTimeout(() => {}, 10000); // 10 seconds timeout
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Image src="/image.png" alt="Loading..." width={300} height={300} />
+      <Image
+        className="animate-pulse"
+        src="/image.png"
+        alt="Loading..."
+        width={300}
+        height={300}
+      />
       <div>
         <p className="text-sm mt-5">Loading incidents...</p>
       </div>
