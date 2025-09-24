3da8595d483747b749e36bfb5c85cf382b3ed810
Diego Massarni <webdiegomassarini@gmail.com>
2025-09-24 11:21:17 +0200

    wip: navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { ToggleTheme } from "./ToggleTheme";
 import ServicenowLogo from "./ServicenowLogo";
+import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="bg-[#032D42]  p-4">
-      <div className="flex items-start justify-between container mx-auto">
-        <ServicenowLogo className="w-44  h-10" />
+      <div className="flex items-center justify-between container mx-auto border-b border-[#0A4A6D] pb-4">
+        <Link href="/">
+          <ServicenowLogo className="w-44  h-10" />
+        </Link>
+
         <ToggleTheme />
+      </div>
+      <div className="container mx-auto mt-4">
+        <Link
+          href="/knowledge"
+          className="text-white text-sm self-end px-1.5 py-1 rounded-sm bg-[#0A4A6D] border-[#0A4A6D] hover:bg-[#01405E] transition-all"
+        >
+          Knowledge Articles
+        </Link>
       </div>
     </div>
   );
